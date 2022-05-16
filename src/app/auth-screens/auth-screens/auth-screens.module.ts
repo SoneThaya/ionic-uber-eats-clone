6767eab98e5ab098dc490c7d081d85a7418a7419
sone <sthaya@gmail.com>
2022-05-15 20:32:19 -0700

    styled auth screen

--- src/app/auth-screens/auth-screens/auth-screens.module.ts
+++ src/app/auth-screens/auth-screens/auth-screens.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -11,10 +11,10 @@ import { AuthScreensPage } from './auth-screens.page';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    AuthScreensPageRoutingModule
+    AuthScreensPageRoutingModule,
   ],
-  declarations: [AuthScreensPage]
+  declarations: [AuthScreensPage],
 })
 export class AuthScreensPageModule {}
